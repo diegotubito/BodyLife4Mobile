89829eeceb3fc213b9ab9adc02f44b4d6af1be97
David Diego Gomez <diegodavid@icloud.com>
2020-04-19 03:12:14 -0300

    Create Application Module

---
+++ Application/Application/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Application
+//
+//  Created by David Diego Gomez on 19/04/2020.
+//  Copyright © 2020 David Diego Gomez. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ Application/Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Application
+//
+//  Created by David Diego Gomez on 19/04/2020.
+//  Copyright © 2020 David Diego Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        #if DEVELOP
+            print("Develop")
+        #else
+            print("Production")
+        #endif
+    }
+
+}
+
